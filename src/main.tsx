0f99cc31aa1d25109ba1b830c478838589d87c5a
ardalanmohajer <ardalanmohajer@gmail.com>
2026-05-02 10:10:33 -0500

    Init

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate, Outlet, useLocation } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import "./styles.css";
+
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+      <BrowserRouter>
+  
+      </BrowserRouter>
+  </React.StrictMode>,
+);
